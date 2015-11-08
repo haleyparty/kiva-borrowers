2dcc938b3d17b80c2309476332822925d7344e53
Haley Bash <halebash@umich.edu>
2015-11-07 17:24:27 -0800

    Add simple dropdown menu and coding comments for later use

--- map.js
+++ map.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   var borrowers = [];
   var map;
   var initMap = function(borrowers) {
+    // **********************
+    // map creation
+    // **********************
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 13, lng: 122},
       zoom: 8
@@ -9,14 +12,22 @@ $(document).ready(function() {
 
     var lat, lng;
     var infoWindow = new google.maps.InfoWindow();
+    // **********************
+    // loop through borrowers
+    // **********************
     $.each(borrowers, function(index, borrower) {
 
-      // get lat/long pairs
+      // **********************
+      // get lat / lng points
+      // **********************
       var pairs = borrower.location.geo.pairs;
       pairs = pairs.split(' ');
       lat = Number(pairs[0]);
       lng = Number(pairs[1]);
 
+      // **********************
+      // create info window template
+      // **********************
       var infoWindow = new google.maps.InfoWindow({
         content: '<h3>' + borrower.name + '</h3> \
                   <p><a href="http://www.kiva.org/lend/' + borrower.id + '?app_id=' + borrower.id + '" target="_blank">Lend Now</a> \
@@ -26,10 +37,12 @@ $(document).ready(function() {
                   <p><b>Use:</b> ' + borrower.use + '</p>'
       });
 
+      // **********************
+      // create marker with listener for info window
+      // **********************
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(lat, lng),
-        map: map,
-        // title: borrower.name + ', ' + borrower.use
+        map: map
       });
 
       marker.addListener('click', function() {
@@ -38,11 +51,13 @@ $(document).ready(function() {
     });
   }
 
-  // get all loans currently raising funds
+  // **********************
+  // loop through borrowers
+  // **********************
   $.getJSON('http://api.kivaws.org/v1/loans/search.json?status=fundraising', function(data) {
     var pages = data.paging.pages;
     var pageNumbers = [];
-    for (var i = 1; i <= 2; i++) {
+    for (var i = 1; i <= 3; i++) {
       pageNumbers.push(i);
     }
     $.each(pageNumbers, function(_, pageNumber) {
@@ -53,7 +68,7 @@ $(document).ready(function() {
         }
         initMap(borrowers);
       });
-    })
+    });
   });
 
 });
