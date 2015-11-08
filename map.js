16f44ae89e0ceeeb2aade109f0e20ebfc3e6de4e
Haley Bash <halebash@umich.edu>
2015-11-07 16:51:34 -0800

    Attempt to get all paginated JSON information

--- map.js
+++ map.js
@@ -1,13 +1,6 @@
-var map;
-// populate borrowers with relevant borrower information
-  // borrower name
-  // lat/long
-  // sector
-  // activity
-  // use
-  // loan amount
 $(document).ready(function() {
   var borrowers = [];
+  var map;
   var initMap = function(borrowers) {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 13, lng: 122},
@@ -17,6 +10,7 @@ $(document).ready(function() {
     var lat, lng;
     var infoWindow = new google.maps.InfoWindow();
     $.each(borrowers, function(index, borrower) {
+
       // get lat/long pairs
       var pairs = borrower.location.geo.pairs;
       pairs = pairs.split(' ');
@@ -25,6 +19,7 @@ $(document).ready(function() {
 
       var infoWindow = new google.maps.InfoWindow({
         content: '<h3>' + borrower.name + '</h3> \
+                  <p><a href="http://www.kiva.org/lend/' + borrower.id + '?app_id=' + borrower.id + '" target="_blank">Lend Now</a> \
                   <p><b>Amount:</b> $' + borrower.loan_amount + '</p> \
                   <p><b>Sector:</b> ' + borrower.sector + '</p> \
                   <p><b>Activity:</b> ' + borrower.activity + '</p> \
@@ -34,7 +29,7 @@ $(document).ready(function() {
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(lat, lng),
         map: map,
-        title: borrower.name
+        // title: borrower.name + ', ' + borrower.use
       });
 
       marker.addListener('click', function() {
@@ -45,11 +40,20 @@ $(document).ready(function() {
 
   // get all loans currently raising funds
   $.getJSON('http://api.kivaws.org/v1/loans/search.json?status=fundraising', function(data) {
-    var loans = data.loans;
-    for (var i = 0; i < loans.length; i++) {
-      borrowers.push(loans[i]);
+    var pages = data.paging.pages;
+    var pageNumbers = [];
+    for (var i = 1; i <= 2; i++) {
+      pageNumbers.push(i);
     }
-    initMap(borrowers);
+    $.each(pageNumbers, function(_, pageNumber) {
+      $.getJSON('http://api.kivaws.org/v1/loans/search.json?status=fundraising&page=' + pageNumber, function(data) {
+        var loans = data.loans;
+        for (var i = 0; i < loans.length; i++) {
+          borrowers.push(loans[i]);
+        }
+        initMap(borrowers);
+      });
+    })
   });
 
 });
