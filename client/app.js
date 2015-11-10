77efd9b96d59ad9bbfa8d6fdb16d7a84f5362ab5
Haley Bash <halebash@umich.edu>
2015-11-09 17:59:55 -0800

    Remove all borrowers that are fully funded

--- client/app.js
+++ client/app.js
@@ -2,28 +2,32 @@
 var makeBorrowerOption = function(loans, amountToLend) {
   var items = [];
   $.each(loans, function(index, loan) {
-    var loanAmount = loan.loan_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    var fundedAmount = loan.funded_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    var leftToFund = (loan.loan_amount - loan.funded_amount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    var contributionPercentage = Math.round((amountToLend / loan.loan_amount) * 100);
-    if (contributionPercentage > 100) {
-      contributionPercentage = 100;
-    }
+    console.log(loan.loan_amount, typeof loan.loan_amount)
+    console.log(loan.funded_amount, typeof loan.funded_amount)
+    if (loan.loan_amount - loan.funded_amount > 0) {
+      var loanAmount = loan.loan_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      var fundedAmount = loan.funded_amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      var leftToFund = (loan.loan_amount - loan.funded_amount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      var contributionPercentage = Math.round((amountToLend / loan.loan_amount) * 100);
+      if (contributionPercentage > 100) {
+        contributionPercentage = 100;
+      }
 
-    items.push('<img src="http://www.kiva.org/img/w200h200/' + loan.image.id + '.jpg"> \
-                <h2>' + loan.name + '</h2> \
-                <p><a href="http://www.kiva.org/lend/' + loan.id + '?app_id=' + loan.id + '" target="_blank">Lend</a></p> \
-                <b>Location:</b> ' + loan.location.town + ', ' + loan.location.country + '<br> \
-                <b>Activity:</b> ' + loan.activity + '<br> \
-                <b>Use:</b> ' + loan.use + '</p> \
-                <p><b>Amount Requested:</b> $' + loanAmount + '<br> \
-                <b>Amount Funded So Far:</b> $' + fundedAmount + '<br> \
-                <b>Amount Left to Fund:</b> $' + leftToFund + '<br> \
-                <b>Contribution Percentage: </b>' + contributionPercentage + '%<br> \
-                <div class="divider"></div>'
-                );
-  });
-  return items.join('');
+      items.push('<img src="http://www.kiva.org/img/w200h200/' + loan.image.id + '.jpg"> \
+                  <h2>' + loan.name + '</h2> \
+                  <p><a href="http://www.kiva.org/lend/' + loan.id + '?app_id=' + loan.id + '" target="_blank">Lend</a></p> \
+                  <b>Location:</b> ' + loan.location.town + ', ' + loan.location.country + '<br> \
+                  <b>Activity:</b> ' + loan.activity + '<br> \
+                  <b>Use:</b> ' + loan.use + '</p> \
+                  <p><b>Amount Requested:</b> $' + loanAmount + '<br> \
+                  <b>Amount Funded So Far:</b> $' + fundedAmount + '<br> \
+                  <b>Amount Left to Fund:</b> $' + leftToFund + '<br> \
+                  <b>Contribution Percentage: </b>' + contributionPercentage + '%<br> \
+                  <div class="divider"></div>'
+                  );
+      }
+    });
+    return items.join('');
 };
 
 // get countries
